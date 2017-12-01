9449a83b842b531229fde9a6b2450e3b48bea3be
Pedro Goncalves <pnag90@gmail.com>
2017-12-01 17:14:29 +0000

    Prod mode

--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -3,11 +3,11 @@ export const PATH = {
     //REST: 'http://192.168.19.112:8090',
     //SOCKET: 'http://192.168.20.73:3001',
     //// dev
-    REST: 'http://192.168.20.73:8080/fcmobile',
-    SOCKET: 'http://192.168.20.73:3001',
+    ///REST: 'http://192.168.20.73:8080/fcmobile',
+    ///SOCKET: 'http://192.168.20.73:3001',
     //// prod
-    //REST: 'https://epm.first-global.com:8543/fcmobile',
-    //SOCKET: 'https://epm.first-global.com:8543/fcmobile',
+    REST: 'https://epm.first-global.com:8543/fcmobile',
+    SOCKET: 'https://epm.first-global.com:8886',
 };
 
 export const ONESIGNAL_CONFIG = {
